ada65bcea84ecb294283582931f68fd1dd9ea74d
AADI JAIN <jainaadi7102@gmail.com>
2022-02-08 18:57:37 +0530

    Create squares-of-a-sorted-array.cpp

---
+++ squares-of-a-sorted-array.cpp
@@ -0,0 +1,12 @@
+// LeetCode: https://leetcode.com/problems/squares-of-a-sorted-array
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            nums[i] = pow(nums[i], 2);
+        }
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
