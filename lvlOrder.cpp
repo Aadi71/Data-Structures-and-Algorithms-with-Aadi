b2ba48e8457e6db0050f71f70d191e4256d7f111
AADI JAIN <jainaadi7102@gmail.com>
2022-02-09 17:28:50 +0530

    Update lvlOrder.cpp

--- lvlOrder.cpp
+++ lvlOrder.cpp
@@ -17,3 +17,35 @@ void lvlOrder(struct Node * root){
         else if(!q.empty()) q.push(NULL);
     }
 }
+
+
+// https://leetcode.com/problems/binary-tree-level-order-traversal/submissions/
+
+class Solution {    
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> v;
+        vector <int> temp;
+        
+        if(!root) return v;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        
+        while(!q.empty()){
+            int n = q.size();
+            for(int i = 0;i<n;i++){
+                TreeNode * node = q.front();
+                q.pop();
+                temp.push_back(node->val);
+                
+                
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }   
+            v.push_back(temp);
+            temp.clear();            
+        }
+        return v;
+    }
+};
