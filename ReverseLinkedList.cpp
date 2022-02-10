149819c3ea5556722b711b62b3dcdf6b19a3bfd6
AADI JAIN <jainaadi7102@gmail.com>
2022-02-10 22:38:29 +0530

    Update ReverseLinkedList.cpp

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -1,3 +1,26 @@
+//https://leetcode.com/problems/reverse-linked-list/
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head == NULL || head->next == NULL) return head;
+        
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        ListNode* current = head;
+        
+        while(current != NULL){
+            next = current -> next;
+            current -> next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        
+        return prev;
+    }
+};
+
 //HackerRank Solution to reverse a LinkedList: 
 
 #include <bits/stdc++.h>
