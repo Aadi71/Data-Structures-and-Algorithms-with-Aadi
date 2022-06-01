92aa525ff16a1ac80c35f12eb5b6769a53b10a95
AADI JAIN <jainaadi7102@gmail.com>
2022-06-01 23:15:24 +0530

    Add files via upload

---
+++ CodeChef/Starters41_June_Div4.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+// Problem 1: https://www.codechef.com/START41D/problems/CBSPEED
+
+int main() {
+	// your code goes here
+	
+	int a, b;
+	cin>>a>>b;
+	
+	if(a<b) cout<<"YES";
+	else cout<<"NO";
+	return 0;
+}
+
+// Problem 2: https://www.codechef.com/START41D/problems/CCHOCOLATES
+
+int main() {
+	int n;
+	cin>>n;
+	
+	while(n--){
+	    int x, y, z;
+	    cin>>x>>y>>z;
+	    
+	    int sum = x*5 + y*10;
+	    cout<<sum/z<<"\n";
+	}
+	return 0;
+}
+
+
+
+// Problem 3: https://www.codechef.com/START41D/problems/WINNERR
+int main() {
+	int n;
+	cin>>n;
+	
+	while(n--){
+	    int pa, pb, qa, qb;
+	    cin>>pa>>pb>>qa>>qb;
+	    
+	    int p = pa > pb ? pa : pb;
+	    int q = qa > qb ? qa : qb;
+	    
+	    if(p>q) cout<<"Q";
+	    else if(p<q) cout<<"P";
+	    else cout<<"TIE";
+	    
+	    cout<<"\n";
+	}
+	return 0;
+}
+
+
+// Problem 4: https://www.codechef.com/START41D/problems/INCREAR
+
+int main() {
+	int n;
+	cin>>n;
+	
+	while(n--){
+	    int a, b;
+	    cin>>a>>b;
+	    
+	    int sum = 0;
+	    if(a == b){
+            cout<<sum<<"\n";
+        }
+        else{
+            if(a > b && (a - b) % 2 == 0) sum = (a - b) / 2;
+
+            else if(a > b) sum = (a - b) / 2 + 2;
+
+            else sum = (b - a); 
+            
+            cout<<sum<<"\n";
+        }
+        
+            
+	}
+	return 0;
+}
+
+
+// Problem 5: https://www.codechef.com/START41D/problems/PATHPAR
+
+// Problem 6: https://www.codechef.com/START41D/problems/ISPAR
+
+// Problem 7: https://www.codechef.com/START41D/problems/ISPAR
+
+// Problem 8: https://www.codechef.com/START41D/problems/DEARRANGE
+
