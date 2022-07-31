ee5ff434bea3fcf8bd943fc51dc14015cb6eeba5
AADI JAIN <jainaadi7102@gmail.com>
2022-07-31 18:44:29 +0530

    Update missing-number.cpp

--- missing-number.cpp
+++ missing-number.cpp
@@ -3,6 +3,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        
+        // Solution 1
         int n = nums.size();
         
         int m = INT_MIN;
@@ -14,5 +16,24 @@ public:
         }
         if(m+1 == nums.size()) return m+1; 
         return ((m * (m+1) / 2) - sum);
+        
+        
+        // Solution 2:
+        int m = *max_element(nums.begin(), nums.end());
+        if(m != nums.size()) return nums.size();
+        vector<int> arr(m+1, 0);
+        for (int i = 0; i < nums.size(); i++)
+            arr[nums[i]] = 1;
+        for (int i = 0; i < m + 1; i++)
+            if (arr[i] == 0)
+                return i;
+        return -1;
+        
+        // Solution 3
+        int n = nums.size();
+        int sum = (n * (n+1)) / 2;
+        int check = 0;
+        for(int i = 0; i<n; i++) check += nums[i];
+        return sum - check;
     }
 };
