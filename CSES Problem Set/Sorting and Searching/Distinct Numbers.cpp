909fe6f260a02e528001326c39e59b896b58a445
AADI JAIN <jainaadi7102@gmail.com>
2022-08-28 18:48:49 +0530

    Update Distinct Numbers.cpp

--- CSES Problem Set/Sorting and Searching/Distinct Numbers.cpp
+++ CSES Problem Set/Sorting and Searching/Distinct Numbers.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <set>
 using namespace std;
 
 int main(){
     cin.sync_with_stdio(false);
-    
+    int n; cin >> n;
+    set<int> s;
+    while(n--){
+        int i; cin >> i;
+        s.insert(i);
+    }
+    cout << s.size();
 }
