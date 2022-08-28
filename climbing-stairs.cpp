2545d26fb3020606aa1bec79205f71af41612ca4
AADI JAIN <jainaadi7102@gmail.com>
2022-08-28 11:24:07 +0530

    Update climbing-stairs.cpp

--- climbing-stairs.cpp
+++ climbing-stairs.cpp
@@ -3,7 +3,6 @@
 
 // Basically, it's a fibonacci series and we just have to return the sum of last two numbers
 
-
 class Solution {
 public:
     int climbStairs(int n) {
@@ -18,5 +17,19 @@ public:
             b = sum;
         }
         return sum;
+    }
+};
+
+// Dynamic Programming
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        int arr[n + 1];
+        for(int i = 0; i<=2; i++) arr[i] = i;
+        
+        for(int i = 3; i<=n; i++) arr[i] = arr[i-1] + arr[i - 2];
+        return arr[n];
     }
 };
