4ed27cb8ee0246bb1bd64dbe1f778a5fe5f8367a
AADI JAIN <jainaadi7102@gmail.com>
2022-09-07 14:30:58 +0530

    Create construct-string-from-binary-tree.cpp

---
+++ construct-string-from-binary-tree.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/construct-string-from-binary-tree/
+
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(!root) return "";
+        if(!root -> left && !root -> right) return to_string(root -> val) + "";
+        if(!root -> right) return root -> val + "(" + tree2str(root -> left) + ")";
+        return root -> val + "(" + tree2str(root -> left) + ")(" + tree2str(root -> right) + ")";
+    }
+};
