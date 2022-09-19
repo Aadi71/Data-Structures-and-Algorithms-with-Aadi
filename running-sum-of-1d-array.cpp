cb2452506e406494968a7b7dc4612bd935198b65
AADI JAIN <jainaadi7102@gmail.com>
2022-09-19 13:44:34 +0530

    Create running-sum-of-1d-array.cpp

---
+++ running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/running-sum-of-1d-array/
+
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        if(nums.size() <= 1) return nums;
+        for(int i = 1; i<nums.size(); i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+};
