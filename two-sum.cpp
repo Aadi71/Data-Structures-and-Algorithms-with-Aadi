be995bdd47fc0beb1c83471e175edaf12338f62e
AADI JAIN <jainaadi7102@gmail.com>
2022-09-20 19:59:08 +0530

    Update two-sum.cpp

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,6 +1,21 @@
 // https://leetcode.com/problems/two-sum/
 
+// Approach 1
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> ind;
+        for (int i = 0; i < nums.size(); i++) {
+            if (ind.find(target - nums[i]) != ind.end())
+                return {ind[target - nums[i]], i};
+            ind[nums[i]] = i;
+        }
+        return {};
+    }
+};
 
+
+// Approach 2
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
