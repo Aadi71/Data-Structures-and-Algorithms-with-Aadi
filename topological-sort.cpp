1fc9fcd94f69de179fb4605c302e71f2c459cf34
AADI JAIN <jainaadi7102@gmail.com>
2022-09-21 21:41:50 +0530

    Update topological-sort.cpp

--- topological-sort.cpp
+++ topological-sort.cpp
@@ -2,6 +2,9 @@
 
 #include <bits/stdc++.h> 
 
+
+// DFS
+
 using namespace std;
 
 void topoSort(int node, vector<bool> &visited, stack<int> &s, unordered_map<int, vector<int>> &adj){
@@ -27,4 +30,38 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
         s.pop();
     }
     return ans;
+}
+
+
+// BFS: Kahn's Algorithm    
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    unordered_map<int, vector<int>> adj;
+    for(int i = 0; i<edges.size(); i++){
+        adj[edges[i][0]].push_back(edges[i][1]);
+    }
+    vector<int> inDegree(v);
+    for(auto x: adj){
+        for(auto y: x.second) inDegree[y]++;
+    }
+    queue<int> q;
+    for(int i = 0; i<v; i++){
+        if(inDegree[i] == 0) q.push(i);   
+    }
+    
+    // BFS
+    vector<int> ans;
+    while(!q.empty()){
+        int f = q.front();
+        q.pop();
+        ans.push_back(f);
+        
+        for(auto x: adj[f]){
+            inDegree[x]--;
+            if(inDegree[x] == 0) q.push(x);
+        }
+    }
+    return ans;
+    
+    
 }
