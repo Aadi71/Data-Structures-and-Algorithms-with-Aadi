68b407fc7e0ffa66a365a528136647590b892795
AADI JAIN <jainaadi7102@gmail.com>
2022-09-22 07:15:46 +0530

    Update reverse-words-in-a-string-iii.cpp

--- reverse-words-in-a-string-iii.cpp
+++ reverse-words-in-a-string-iii.cpp
@@ -10,7 +10,6 @@ public:
     }
     string reverseWords(string s) {
         string str = "";
-        stack<string> st;
         for(int i = 0; i<s.length(); i++){
             string temp = "";
             while(i < s.length() && s[i] != ' ' && i != s.length())
