6c655790f44f857726266557bc6a39d7c9669eb3
AADI JAIN <jainaadi7102@gmail.com>
2022-09-22 06:55:49 +0530

    Create reverse-words-in-a-string-iii.cpp

---
+++ reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+class Solution {
+public:
+    string reverse(string s){
+        int i = 0, j = s.length() - 1;
+        while(i < j)
+            swap(s[i++], s[j--]);
+        return s;
+    }
+    string reverseWords(string s) {
+        string str = "";
+        stack<string> st;
+        for(int i = 0; i<s.length(); i++){
+            string temp = "";
+            while(i < s.length() && s[i] != ' ' && i != s.length())
+                temp += s[i++];
+            str += reverse(temp) + " ";
+        }
+        return str.substr(0, str.length() - 1);
+    }
+};
