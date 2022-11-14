34dbcff4b5bf66e7959f5082a928b36786bbe7da
AADI JAIN <jainaadi7102@gmail.com>
2022-11-15 01:09:48 +0530

    Update rotting-oranges.cpp

--- rotting-oranges.cpp
+++ rotting-oranges.cpp
@@ -1,5 +1,55 @@
 // https://leetcode.com/problems/rotting-oranges/
 
+// Optimised and efficient approach
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        int row = grid.size();
+        int col = grid[0].size();
+        int i = 0, j = 0;
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
+                if(grid[i][j] == 2) {
+                    q.push({i, j});
+                }
+            }
+        }
+        int time = 0;
+        int a[] = {0, 0, -1, 1};
+        int b[] = {-1, 1, 0, 0};
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+                for(int k = 0; k<4; k++){
+                    int inew = i + a[k];
+                    int jnew = j + b[k];
+                    if(inew >= 0 && jnew >= 0 && inew < row && jnew < col && grid[inew][jnew] == 1){
+                        grid[inew][jnew] = 2;
+                        q.push({inew, jnew});
+                    }
+                }
+            }
+            time++;
+        }
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
+                if(grid[i][j] == 1)
+                    return -1;
+            }
+        }
+        if(time == 0) return 0;
+        return time - 1;
+    }
+};
+
+
+
+// Naive - First Approach
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
