e1588907b4c2c0bbe689a36b93e96c010d2233ad
AADI JAIN <jainaadi7102@gmail.com>
2022-11-17 19:45:59 +0530

    Update as-far-from-land-as-possible.cpp

--- as-far-from-land-as-possible.cpp
+++ as-far-from-land-as-possible.cpp
@@ -1,5 +1,36 @@
 // https://leetcode.com/problems/as-far-from-land-as-possible/
 
+// Optimized and Fast Approach
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == 1) continue;
+                grid[i][j] = 201;
+                if(i > 0) grid[i][j] = min(grid[i][j], grid[i - 1][j] + 1);
+                if(j > 0) grid[i][j] = min(grid[i][j], grid[i][j - 1] + 1);
+            }
+        }
+        for(int i = n -1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(grid[i][j] == 1) continue;
+                if(i < n - 1) grid[i][j] = min(grid[i][j], grid[i + 1][j] + 1);
+                if(j < n - 1) grid[i][j] = min(grid[i][j], grid[i][j + 1] + 1);
+                ans = max(grid[i][j], ans);
+            }
+        }
+        return ans == 201 ? -1 : ans - 1;
+    }
+};
+
+
+
+
+// BFS approach
+
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
