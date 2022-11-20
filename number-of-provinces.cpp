fa0cac24a658cfe00ee98a75638ea14834681c2f
AADI JAIN <jainaadi7102@gmail.com>
2022-11-20 18:14:22 +0530

    Update number-of-provinces.cpp

--- number-of-provinces.cpp
+++ number-of-provinces.cpp
@@ -1,24 +1,26 @@
-// https://practice.geeksforgeeks.org/problems/number-of-provinces/1
+// https://leetcode.com/problems/number-of-provinces/
 
 class Solution {
-    void dfs(int node, vector<vector<int>> adj, vector<bool> &vis){
-        vis[node] = true;
-        for(int i = 0; i<adj[node].size(); i++){
-            if(adj[node][i] != 0 && !vis[i]){
-                dfs(i, adj, vis);
+public:
+    void DFS(vector<vector<int>>& isConnected, vector<int> &vis, int i){
+        vis[i] = 1;
+        for(int j = 0; j<isConnected[i - 1].size(); j++){
+            if(isConnected[i - 1][j] == 1 && !vis[j + 1]){
+                vis[j + 1] = 1;
+                DFS(isConnected, vis, j + 1);
             }
         }
     }
-  public:
-    int numProvinces(vector<vector<int>> adj, int V) {
-        vector<bool> vis(V, false);
-        int sum = 0;
-        for(int i = 0; i<V; i++){
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n + 1, 0);
+        int ans = 0;
+        for(int i = 1; i<=n; i++){
             if(!vis[i]){
-                dfs(i, adj, vis);
-                sum++;
+                DFS(isConnected, vis, i);
+                ans++;
             }
         }
-        return sum;
+        return ans;
     }
 };
