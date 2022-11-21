236350653da8df5f95624229ffb2acdd140a3891
AADI JAIN <jainaadi7102@gmail.com>
2022-11-21 11:52:49 +0530

    Create n-th-tribonacci-number.cpp

---
+++ n-th-tribonacci-number.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/n-th-tribonacci-number/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n <= 1) return n;
+        if(n == 2) return 1;
+        vector<int> v(n + 1, 0);
+        v[0] = 0; v[1] = 1; v[2] = 1;
+        for(int i = 3; i<=n; i++)
+            v[i] = v[i - 1] + v[i - 2] + v[i - 3];
+        return v[n];
+    }
+};
