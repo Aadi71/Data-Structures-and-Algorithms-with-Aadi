204ea4f27a6fae6ec839140b80152c680fa3c18a
AADI JAIN <jainaadi7102@gmail.com>
2022-12-21 19:19:44 +0530

    Update number-of-islands.cpp

--- number-of-islands.cpp
+++ number-of-islands.cpp
@@ -1,5 +1,35 @@
 // https://leetcode.com/problems/number-of-islands/
 
+// DFS
+class Solution {
+public:
+    void DFS(vector<vector<char>>& grid, int i, int j){
+        grid[i][j] = '2';
+        int a[] = {-1, 1, 0, 0};
+        int b[] = {0, 0, -1, 1};
+        for(int k = 0; k<4; k++){
+            int inew = i + a[k];
+            int jnew = j + b[k];
+            if(inew >= 0 && jnew >= 0 && inew < grid.size() && jnew < grid[0].size() && grid[inew][jnew] == '1') DFS(grid, inew, jnew);
+        }
+        return;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int sum = 0;
+        for(int i = 0; i<grid.size(); i++){
+            for(int j = 0; j<grid[0].size(); j++){
+                if(grid[i][j] == '1'){
+                    DFS(grid, i, j);
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
+};
+
+
+// BFS
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
