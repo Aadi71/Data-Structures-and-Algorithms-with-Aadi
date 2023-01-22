ace32364502eb63de203ce81d4224212887bafb3
AADI JAIN <jainaadi7102@gmail.com>
2023-01-22 10:49:00 +0530

    Update maximum-subarray.cpp

--- maximum-subarray.cpp
+++ maximum-subarray.cpp
@@ -1,6 +1,5 @@
 // https://leetcode.com/problems/maximum-subarray/submissions/
 // Kadane's Algorithm
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -9,9 +8,22 @@ public:
         for(int i = 0; i< nums.size(); i++){
             if(sum < 0) sum =0;
             sum += nums[i];
-            
             maxSum = max(sum, maxSum);
         }
         return maxSum;
+    }
+};
+
+// DP Approach
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            nums[i] = nums[i] + (nums[i-1] > 0 ? nums[i - 1] : 0);
+            sum = max(sum, nums[i]);
+        }
+        return sum;
     }
 };
