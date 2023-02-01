b9f691c4cb13080dc5cf749eafbcb475f6efc6f0
AADI JAIN <jainaadi7102@gmail.com>
2023-02-01 13:15:01 +0530

    Update pascals-triangle.cpp

--- pascals-triangle.cpp
+++ pascals-triangle.cpp
@@ -3,20 +3,14 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> v;
-        
+        vector<vector<int>> ans;
         for(int i = 0; i<numRows; i++){
-            vector<int> temp(i+1, 1);
-            
-            if(i >1){
-                int k = 0;
-                for(int j = 1; j<i; j++){
-                    temp[j] = v[i-1][k++] + v[i-1][k];
-                }
+            vector<int> temp(i + 1, 1);
+            for(int j = 1; j<i; j++){
+                temp[j] = ans[i - 1][j] + ans[i - 1][j - 1];
             }
-            
-            v.push_back(temp);
+            ans.push_back(temp);
         }
-        return v;
+        return ans;
     }
 };
