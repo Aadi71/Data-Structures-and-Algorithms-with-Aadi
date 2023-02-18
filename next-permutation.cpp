33667d644e8ab0d3643242a625232c82d8367c53
AADI JAIN <jainaadi7102@gmail.com>
2023-02-18 19:27:39 +0530

    Update next-permutation.cpp

--- next-permutation.cpp
+++ next-permutation.cpp
@@ -3,37 +3,14 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int len = nums.size();
-		int first = 0;
-		if(len == 1){
-			return ;
-		}
-		int i = len - 2;
-		while( i >= 0 && nums[i] >=nums[i+1] ){
-			i--;
-		}
-		if(i == -1){
-			sort(nums.begin(),nums.end());
-			return;
-		}
-
-		int j = len - 1;
-		while( nums[i] >= nums[j]){
-			j--;
-
-		}
-		int temp = nums[j];
-		nums[j] = nums[i];
-		nums[i] = temp;
-
-		first = i+ 1;    
-		int last = len - 1;
-		while( first < last ){  
-			int temp = nums[first];
-			nums[first] = nums[last];
-			nums[last] = temp  ;
-			first++;
-			last--;
-		}
-	}
+        if(nums.size() <= 1) return;
+        int i = nums.size() - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1]) i--;
+        if(i >= 0){
+            int j = nums.size() - 1;
+            while(nums[j] <= nums[i]) j--;
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+    }
 };
