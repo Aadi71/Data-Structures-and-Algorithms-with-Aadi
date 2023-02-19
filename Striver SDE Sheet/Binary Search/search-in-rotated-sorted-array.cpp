bdf38e43d2ada2c6407c5bafa739d9b0eb22e245
AADI JAIN <jainaadi7102@gmail.com>
2023-02-19 16:15:15 +0530

    Create search-in-rotated-sorted-array.cpp

---
+++ Striver SDE Sheet/Binary Search/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            if(nums[low] <= nums[mid]){
+                if(nums[mid] >= target && nums[low] <= target) high = mid - 1;
+                else low = mid + 1;
+            }
+            else{
+                if(nums[mid] <= target && nums[high] >= target) low = mid +1;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
