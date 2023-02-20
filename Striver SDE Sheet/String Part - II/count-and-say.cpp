59e3bd1d92e88588f6316395f4c0cfe723272264
AADI JAIN <jainaadi7102@gmail.com>
2023-02-20 15:01:54 +0530

    Create count-and-say.cpp

---
+++ Striver SDE Sheet/String Part - II/count-and-say.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/count-and-say/
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 0) return "";
+        string res = "1";
+        while(--n){
+            string temp = "";
+            for(int i = 0; i<res.size(); i++){
+                int count = 1;
+                while((i + 1 < res.size()) && (res[i] == res[i + 1])){
+                    count++;
+                    i++;
+                }
+                temp += to_string(count) + res[i];
+            }
+            res = temp;
+        }
+        return res;
+    }
+};  
