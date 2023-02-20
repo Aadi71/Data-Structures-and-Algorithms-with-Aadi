e25db6fb6ce76bf7c6024251e3593bb3727d0513
AADI JAIN <jainaadi7102@gmail.com>
2023-02-20 23:07:09 +0530

    Create linked-list-cycle-ii.cpp

---
+++ Striver SDE Sheet/Linked List Part - ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head -> next == NULL) return NULL;
+        ListNode* slow = head, * fast = head;
+
+        while(fast -> next && fast -> next -> next){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow -> next;
+                    fast = fast -> next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
