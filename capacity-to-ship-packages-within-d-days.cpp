2e1ee7574d0a35db0ef84f8222df64a45cc161b3
AADI JAIN <jainaadi7102@gmail.com>
2023-02-22 23:17:38 +0530

    Update capacity-to-ship-packages-within-d-days.cpp

--- capacity-to-ship-packages-within-d-days.cpp
+++ capacity-to-ship-packages-within-d-days.cpp
@@ -1,5 +1,32 @@
 // https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
 
+// Brute Force, Time Limit Exceeded. TC: O(N^2)
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int total = 0, maxElement = 0;
+        for(int i = 0; i < weights.size(); i++){
+            total += weights[i];
+            maxElement = maxElement > weights[i] ? maxElement : weights[i];
+        }
+        int low = maxElement, high = total;
+        
+        for(int i = maxElement; i <= total; i++){
+            int d = 1, temp = 0;
+            for(int j = 0; j < weights.size(); j++){
+                temp += weights[j];
+                if(temp > i){
+                    d++;
+                    temp = weights[j];
+                }
+            }
+            if(d <= days) return i;
+        }
+        return -1;
+    }
+};
+
+// Optimized Solution, Reducing search space by Binary Search. TC: O(NlogN)
 class Solution {
 public:
     bool validity(int w, vector<int> &weights, int days){
