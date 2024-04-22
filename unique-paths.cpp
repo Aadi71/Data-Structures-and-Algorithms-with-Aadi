057c057f4269b9654a07ea431cd36af1149d1f78
AADI JAIN <jainaadi7102@gmail.com>
2024-04-22 14:12:05 +0530

    Update unique-paths.cpp

--- unique-paths.cpp
+++ unique-paths.cpp
@@ -3,37 +3,28 @@
 // Recursive Solution - Giving TLE. TC - O(2^(n X m))
 class Solution {
 public:
-    bool valid(int i, int j, int m, int n){
-        if(m >= 0 && n >= 0 && i<m && j <n) return true;
-        return false;
-    }
-    void solve(int i, int j, int m, int n, int &sum){
-        if(i == m - 1 && j == n - 1){
-            sum++;
-            return;
-        }
-        if(valid(i + 1, j, m, n)) solve(i + 1, j, m, n, sum);
-        if(valid(i, j + 1, m, n)) solve(i, j + 1, m, n, sum);
+    int solve(int m, int n, int i, int j) {
+        if (i == (m - 1) && j == (n - 1)) return 1;
+        else if (i >= m || j >= n) return 0;
+        else return solve(m, n, i, j + 1) + solve(m, n, i + 1, j);
     }
     int uniquePaths(int m, int n) {
-        int sum = 0;
-        solve(0, 0, m, n, sum);
-        return sum;
+        return solve(m, n, 0, 0);
     }
 };
 
 // Memoization Solution - Accepted. TC - O(n X m) 
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>> &dp){
-        if(i == m - 1 && j == n - 1) return 1;
-        if(i < 0 || j < 0 || i >= m || j >= n) return 0;
-        if(dp[i][j] != -1) return dp[i][j]; 
-        return dp[i][j] = (solve(i + 1, j, m, n, dp) + solve(i, j + 1, m, n, dp));
-    } 
+    int solve(int m, int n, int i, int j, vector<vector<int>> &dp) {
+        if (i == (m - 1) && j == (n - 1)) return 1;
+        else if (i >= m || j >= n) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        else return dp[i][j] = solve(m, n, i, j + 1, dp) + solve(m, n, i + 1, j, dp);
+    }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n, -1));
-        return solve(0, 0, m, n, dp);
+        return solve(m, n, 0, 0, dp);
     }
 };
 
@@ -41,15 +32,33 @@ public:
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, -1));
+        vector<vector<int>> dp(m, vector<int> (n, 0));
         dp[m - 1][n - 1] = 1;
-        for(int i = 0; i<m; i++) dp[i][n - 1] = 1;
-        for(int j = 0; j<n; j++) dp[m - 1][j] = 1;
-        for(int i = m - 2; i >= 0; i--){
-            for(int j = n - 2; j>= 0; j--){
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if (i + 1 < m) dp[i][j] += dp[i + 1][j];
+                if (j + 1 < n) dp[i][j] += dp[i][j + 1];
             }
         }
         return dp[0][0];
+    }
+};
+
+
+// Tabulation : Space Optimized O(N)
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> curr (n, 0);
+        vector<int> prev (n, 0);
+        curr[n - 1] = 1;
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if (i + 1 < m) curr[j] = prev[j];
+                if (j + 1 < n) curr[j] += curr[j + 1];
+            }
+            prev = curr;
+        }
+        return curr[0];
     }
 };
