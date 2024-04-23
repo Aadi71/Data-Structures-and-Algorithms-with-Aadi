7844c6baa5ef3dfa1e6e8b064724334fb3d617f4
AADI JAIN <jainaadi7102@gmail.com>
2024-04-23 23:56:28 +0530

    Update longest-common-subsequence.cpp

--- Striver SDE Sheet/Dynamic Programming/longest-common-subsequence.cpp
+++ Striver SDE Sheet/Dynamic Programming/longest-common-subsequence.cpp
@@ -3,29 +3,28 @@
 // Recursive, Giving TLE
 class Solution {
 public:
-    int solve(int i, string a, int j, string b){
-        if(i < 0 || j < 0) return 0;
-        if(a[i] == b[j]) return 1 + solve(i - 1, a, j - 1, b);
-        else return max(solve(i, a, j - 1, b), solve(i - 1, a, j, b));
+    int solve(string text1, string text2, int i, int j) {
+        if (i >= text1.size() || j >= text2.size()) return 0;
+        if (text1[i] == text2[j]) return 1 + solve(text1, text2, i + 1, j + 1);
+        return max(solve(text1, text2, i + 1, j), solve(text1, text2, i, j + 1));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.length(), vector<int> (text2.length(), -1));
-        return solve(text1.length() - 1, text1, text2.length() - 1, text2);
+        return solve(text1, text2, 0, 0);
     }
 };
 
 // Memoization
 class Solution {
 public:
-    int solve(int i, string a, int j, string b, vector<vector<int>> &dp){
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(a[i] == b[j]) return dp[i][j] = 1 + solve(i - 1, a, j - 1, b, dp);
-        else return dp[i][j] = max(solve(i, a, j - 1, b, dp), solve(i - 1, a, j, b, dp));
+    int solve(string text1, string text2, int i, int j, vector<vector<int>> &dp) {
+        if (i >= text1.size() || j >= text2.size()) return 0;
+        if (text1[i] == text2[j]) return 1 + solve(text1, text2, i + 1, j + 1, dp);
+        if (dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = max(solve(text1, text2, i + 1, j, dp), solve(text1, text2, i, j + 1, dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.length(), vector<int> (text2.length(), -1));
-        return solve(text1.length() - 1, text1, text2.length() - 1, text2, dp);
+        vector<vector<int>> dp (text1.size(), vector<int> (text2.size(), -1));
+        return solve(text1, text2, 0, 0, dp);
     }
 };
 
@@ -33,32 +32,30 @@ public:
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.length() + 1, vector<int> (text2.length() + 1, -1));
-        for(int i = 0; i <= text1.length(); i++) dp[i][0] = 0;
-        for(int j = 0; j <= text2.length(); j++) dp[0][j] = 0;
-        for(int i = 1; i <= text1.length(); i++){
-            for(int j = 1; j <= text2.length(); j++){
-                if(text1[i - 1] == text2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+        vector<vector<int>> dp (text1.size() + 1, vector<int> (text2.size() + 1, 0));
+        for(int i = 1; i <= text1.size(); i++) {
+            for(int j = 1; j <= text2.size(); j++) {
+                if (text1[i - 1] == text2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
                 else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-        return dp[text1.length()][text2.length()];
-    } 
+        return dp[text1.size()][text2.size()];
+    }
 };
 
 // Space Optimized
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        vector<int> prev(text2.length() + 1, 0), curr(text2.length() + 1, 0);
-        for(int i = 1; i <= text1.length(); i++){
-            for(int j = 1; j <= text2.length(); j++){
-                if(text1[i - 1] == text2[j - 1]) curr[j] = 1 + prev[j - 1];
+        vector<int> curr(text2.size() + 1, 0);
+        vector<int> prev(text2.size() + 1, 0);
+        for(int i = 1; i <= text1.size(); i++) {
+            for(int j = 1; j <= text2.size(); j++) {
+                if (text1[i - 1] == text2[j - 1]) curr[j] = 1 + prev[j - 1];
                 else curr[j] = max(prev[j], curr[j - 1]);
             }
             prev = curr;
         }
-        return prev[text2.length()];
-    } 
+        return curr[text2.size()];
+    }
 };
-
