42ccc02fdec5b11566cd7a6cee6308e230e61141
AADI JAIN <jainaadi7102@gmail.com>
2024-04-26 10:54:39 +0530

    Update minimum-falling-path-sum.cpp

--- minimum-falling-path-sum.cpp
+++ minimum-falling-path-sum.cpp
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/minimum-falling-path-sum/
 
+// Tabulation
+class Solution {
+public:
+    int solve(int n, int i, int j, vector<vector<int>>& matrix) {
+        if (i == n) return 0;
+        int a = INT_MAX, c = INT_MAX, b;
+        if (j - 1 >= 0) a = solve(n, i + 1, j - 1, matrix);
+        b = solve(n, i + 1, j, matrix);
+        if (j + 1 < n) c = solve(n, i + 1, j + 1, matrix);
+        return matrix[i][j] + min(a, min(b, c));
+    }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int ans = INT_MAX;
+        for(int j = 0; j < n; j++) {
+            ans = min(ans, solve(n, 0, j, matrix));
+        }
+        return ans;
+    }
+};
+
 // DP + Memoization with SC: O(n X n)
 class Solution {
 public:
@@ -24,28 +45,24 @@ public:
     }
 };
 
-// Space Optimized with SC: O(n)
+// Space Optimized, without any extra space O(1)
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<int> dp(n, 101);
-        vector<int> prev(n, 101);
-        for(int j = 0; j<n; j++) prev[j] = matrix[n - 1][j];
-        for(int i = n - 2; i >= 0; i--){
-            for(int j = 0; j < n; j++){
-                int l, d, r;
-                if(j - 1 >= 0) l = matrix[i][j] + prev[j - 1];
-                else l = INT_MAX;
-                if(j + 1 < n) r = matrix[i][j] + prev[j + 1];
-                else r = INT_MAX;
-                d = matrix[i][j] + prev[j];
-                dp[j] = min(l, min(d, r));
-            }
-            prev = dp;
-        }
         int ans = INT_MAX;
-        for(int i = 0; i < n; i++) ans = min(ans, prev[i]);
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j < n; j++) {
+                int a = INT_MAX, c = INT_MAX, b;
+                if (j - 1 >= 0) a = matrix[i + 1][j - 1];
+                b = matrix[i + 1][j];
+                if (j + 1 < n) c = matrix[i + 1][j + 1];
+                matrix[i][j] = matrix[i][j] + min(a, min(b, c));
+            }
+        }
+        for(int j = 0; j < n; j++) {
+            ans = min(ans, matrix[0][j]);
+        }
         return ans;
     }
 };
